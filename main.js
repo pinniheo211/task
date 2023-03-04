45947fe912c76f383d289c56ea3167b988c5ed9b
pinniheo211 <84260434+pinniheo211@users.noreply.github.com>
2023-03-05 01:36:53 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+ // nav__bar responsive
+hamburger = document.querySelector('.hamburger');
+hamburger.onclick = function(){
+    navBar = document.querySelector('.nav__bar');
+    navBar.classList.toggle('active');
+}
